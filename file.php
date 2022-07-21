cf31559d0954cf408b135a4925f478a990e17ddb
Yasz Avellia <zakamaragames@gmail.com>
2022-07-21 21:00:52 +0700

    membuat variable

--- file.php
+++ file.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hello World"
+$variable = "yasz";
+$namaPanjang = "Muhammad Ilyasa";
+$nickname = "Yasz";
+$age = 15;
+
+echo "Hello World";
+echo "\n";
+
+
+echo $variable;
+echo "\n";
+echo $namaPanjang;
+echo "\n";
+echo $nickname;
+echo "\n";
+echo $age;
+?>

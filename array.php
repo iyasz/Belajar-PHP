70b14d6a49a472cc5d2125b99d1bd56a0fc21d52
Yasz Avellia <zakamaragames@gmail.com>
2022-07-22 21:19:40 +0700

    menambah map array

--- array.php
+++ array.php
@@ -1,10 +1,26 @@
 <?php
 
 $array = ["iyasz", 15, true, "Elaina"];
-var_dump($array[2]);
+// var_dump($array[2]);
 
 $array[2] = "mengganti";
-var_dump($array[2]);
+// var_dump($array[2]);
 
 $array[] = "menambah";
-var_dump($array[4]);
+// var_dump($array[4]);
+
+$yasz = ["hallo", 12, false, true, null];
+var_dump($yasz[0]);
+
+$yasz[2] = "ini mengganti";
+var_dump($yasz[2]);
+
+//map di php
+
+$mapTest = [
+    "nama" => "Yasz Avellia",
+    "umur" => 15,
+    "kelas" => "XI RPL"
+];
+
+// var_dump($mapTest["umur"]);

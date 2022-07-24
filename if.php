8742657bcd1e79e031bbd8bca67baba3e572a44e
Yasz Avellia <zakamaragames@gmail.com>
2022-07-24 13:56:26 +0700

    menambah statment if simple

--- if.php
+++ if.php
@@ -1,1 +1,12 @@
 <?php
+
+$nilaiUjian = 70;
+$nilaiHarian = 85;
+
+if ($nilaiHarian >= 85 && $nilaiUjian <= 70) {
+    echo "Kamu  Lulus!" . PHP_EOL;
+} else {
+    echo "Kamu Tidak Lulus!";
+}
+
+?>

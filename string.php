240ee825342d79d6a15afa2ed6c91d2c542ecb29
Yasz Avellia <zakamaragames@gmail.com>
2022-07-24 13:36:14 +0700

    menambah data string

--- string.php
+++ string.php
@@ -3,4 +3,10 @@
 $firstName = "iyasz";
 $lastName = "Avellia";
 
-$fullName = 
+$fullName = $firstName . $lastName . PHP_EOL;
+// echo $fullName;
+
+$name = "Yasz AVellia";
+// echo "hallo $name Welcome to new world";
+
+echo "Selamat Datang {$name}'s in our home";

85f218ea902f353a7a08bec9ae1eed8a1b004520
Yasz Avellia <zakamaragames@gmail.com>
2022-07-24 14:09:27 +0700

    menambah if else statment

--- if.php
+++ if.php
@@ -3,10 +3,23 @@
 $nilaiUjian = 70;
 $nilaiHarian = 85;
 
-if ($nilaiHarian >= 85 && $nilaiUjian <= 70) {
-    echo "Kamu  Lulus!" . PHP_EOL;
+// if ($nilaiHarian >= 85 && $nilaiUjian <= 70) {
+//     echo "Kamu  Lulus!" . PHP_EOL;
+// } else {
+//     echo "Kamu Tidak Lulus!" . PHP_EOL;
+// }
+
+$p = 71;
+$q = 61;
+
+if ($p >= 90 && $q >= 80) {
+    echo "Pertama";
+} else if ($p > 80 && $q > 70) {
+    echo "kedua";
+} else if ($p > 70 && $q > 60) {
+    echo "ketiga";
 } else {
-    echo "Kamu Tidak Lulus!";
-}
+    echo "keempat";
+};
 
 ?>

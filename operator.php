5b8062cec0973b941c8ebf5a91754ae36e2b60ae
Yasz Avellia <zakamaragames@gmail.com>
2022-07-24 09:48:31 +0700

    menambah operator aritmatika

---
+++ operator.php
@@ -0,0 +1,8 @@
+<?php
+$aritmatika = 24 + 23;
+$pengurangan = 73 - 23;
+$pembagian = 45 / 5;
+$perkalian = 3 * 4;
+$pangkat = 3**5;
+$total = $aritmatika + $pengurangan + $pembagian + $perkalian + $pangkat;
+var_dump($total);

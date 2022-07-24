e3197e4546e73e96c02ad11efacbf6c8dd892a86
Yasz Avellia <zakamaragames@gmail.com>
2022-07-24 09:52:55 +0700

    menambah operator penugasan

--- operator.php
+++ operator.php
@@ -6,3 +6,14 @@ $perkalian = 3 * 4;
 $pangkat = 3**5;
 $total = $aritmatika + $pengurangan + $pembagian + $perkalian + $pangkat;
 var_dump($total);
+
+//operator penugasan
+
+$penugasan = 13;
+$kali = 34;
+$bagi = 26;
+$kurang = 12;
+$penugasan += $kali;
+$penugasan /= $bagi;
+$penugasan -= $kurang;
+var_dump($penugasan);

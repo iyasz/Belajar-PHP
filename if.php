733e4ed69183d671d3db73389bafd9a9a5f7b9dc
Yasz Avellia <zakamaragames@gmail.com>
2022-07-26 21:15:55 +0700

    belajar if else A

--- if.php
+++ if.php
@@ -12,14 +12,27 @@ $nilaiHarian = 85;
 $p = 71;
 $q = 61;
 
-if ($p >= 90 && $q >= 80) {
-    echo "Pertama";
-} else if ($p > 80 && $q > 70) {
-    echo "kedua";
-} else if ($p > 70 && $q > 60) {
-    echo "ketiga";
+// if ($p >= 90 && $q >= 80) {
+//     echo "Pertama";
+// } else if ($p > 80 && $q > 70) {
+//     echo "kedua";
+// } else if ($p > 70 && $q > 60) {
+//     echo "ketiga";
+// } else {
+//     echo "keempat";
+// };
+
+$nilaiA = 74;
+$nilaiB = 80;
+$lulus = "Selamat Kamu Lulus Ujian";
+
+if($nilaiA >= 75 && $nilaiB >= 78) {
+    echo "$lulus";
+} else if ($nilaiA >= 65 && $nilaiB >= 65) {
+    echo "Kamu Remedial ";
 } else {
-    echo "keempat";
-};
+    echo "TIdak Lulus";
+}
+
 
 ?>

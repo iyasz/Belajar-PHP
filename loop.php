30e9e6c255928b916686d5eae8c3ed2c9461f401
Yasz Avellia <zakamaragames@gmail.com>
2022-07-27 21:09:17 +0700

    latihan for loop pengurangan

--- loop.php
+++ loop.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 // for ($angka = 1; $angka <= 20; $angka++) {
 //     echo "ini adalah anka ke-$angka" . PHP_EOL;
 // }
@@ -8,6 +10,10 @@
 //     echo " ke-$nomor";
 // }
 
-for ($lo = 10; $lo >= 1; $lo--) {
-    echo "pengurangan ke-$lo";
+// for ($lo = 10; $lo >= 1; $lo--) {
+//     echo "pengurangan ke-$lo";
+// }
+
+for ($in = 20; $in >= 1; $in--) {
+    echo "$in";
 }

22d2d8d6210a11fa40f7d83de509e51045a5f9d5
Yasz Avellia <zakamaragames@gmail.com>
2022-07-27 20:57:27 +0700

    latihan for loop B

--- loop.php
+++ loop.php
@@ -1,5 +1,9 @@
 <?php
 
-for ($angka = 1; $angka <= 20; $angka++) {
-    echo "ini adalah anka ke-$angka" . PHP_EOL;
+// for ($angka = 1; $angka <= 20; $angka++) {
+//     echo "ini adalah anka ke-$angka" . PHP_EOL;
+// }
+
+for($nomor = 5; $nomor <= 20; $nomor++){
+    echo " ke-$nomor";
 }

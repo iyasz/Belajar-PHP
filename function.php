31a1fc8d57c2fe1158b3bc9ef9ef4a57c1dcb78a
Yasz Avellia <zakamaragames@gmail.com>
2022-07-30 03:51:24 +0700

    menambah function argument

---
+++ function.php
@@ -0,0 +1,20 @@
+<?php 
+
+// function sayHello(){
+//     echo "hello function" . PHP_EOL;
+// }
+
+// echo sayHello();
+
+// if(false){
+//     function sayHai(){
+//         echo "Hai";
+//     }
+// }
+
+function sayHy($name){
+    echo "Hy $name" . PHP_EOL;
+};
+
+sayHy("iyasz");
+sayHy("elaina");

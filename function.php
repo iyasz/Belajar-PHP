cf48688c9998e1a97cb1c656f333b976a72bff09
Yasz Avellia <zakamaragames@gmail.com>
2022-07-31 20:06:52 +0700

    latihan funciton + if

--- function.php
+++ function.php
@@ -16,3 +16,17 @@
 //     echo "Hy $name" . PHP_EOL;
 // };
 
+
+function Pertama(){
+    $nilai = 90; 
+    $absen = 80;
+    if($nilai >= 95 && $absen >= 80){
+        return "Kamu Lulus";
+    } else if($nilai >= 80 && $absen >= 76) {
+        return "Kamu Remedial";
+    } else {
+        return 'Haha';
+    }
+};
+
+var_dump(Pertama());
